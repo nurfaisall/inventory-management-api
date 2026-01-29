defaa45b163126fbefa9a046fd4cd1de3c2e54b5
nurfaisall <nurfaisal1504@gmail.com>
2026-01-29 13:54:50 +0700

    feat: global exception handler and error response standard

--- src/main/java/com/example/inventory_management_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/inventory_management_api/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.inventory_management_api.exception;
 
 import com.example.inventory_management_api.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,10 +17,31 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(400,ex.getMessage()));
     }
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFound(BadRequestException ex){
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UsernameNotFoundException ex){
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(404,ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(
+            MethodArgumentNotValidException ex) {
+
+        String message = ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse(400, message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(500, "Internal server error"));
     }
 
 }
